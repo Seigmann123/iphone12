0e2a3c24779c2b6d91f5bd8d8225cc3d77e1fc07
Sander Seiersnes <sander.seiersnes@gmail.com>
2021-03-10 15:18:30 +0100

    fix ish?

--- js/phone.js
+++ js/phone.js
@@ -1,7 +1,21 @@
-function screen() { 
+/*function screen() { 
     var e = document.getElementById("screen");
     var c = window.getComputedStyle(e).backgroundColor;
     if (c === "rgb(0, 0, 0)") {
     document.getElementById("screen").style.background = "#ffffff";
-} 
+} else{
+    document.getElementById("screen").style.background = "#000";
 }
+}
+*/
+
+function screen() {
+    var e = document.getElementById("screen");
+    e.classList.toggle("white-screen")
+}
+
+function offbutton() {
+    var r = document.getElementsByClassName('off-on-button');
+    r.classList.toggle("white-screen")
+}
+

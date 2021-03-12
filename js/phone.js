c07d5835a3ef6ea55383b76a26a1f1ea6f91d670
Sander Seiersnes <sander.seiersnes@gmail.com>
2021-03-12 13:57:48 +0100

    nofix

--- js/phone.js
+++ js/phone.js
@@ -25,6 +25,12 @@ function turnOn() {
 // }
 
 
+
+
+
+
+
+
 function activateScreenClock() {
     var date = new Date();
     var h = date.getHours();
@@ -44,16 +50,35 @@ function activateScreenClock() {
 
 
     var time = h + ":" + m + " ";
+
+
+    const d = new Date();
+    var options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    
+    
+    // console.log(date.toLocaleDateString("default", options));
+    
+    
+    
+    document.getElementById("date").innerText = options;
+    document.getElementById("date").textContent = options;
     document.getElementById("clock").innerText = time;
     document.getElementById("clock").textContent = time;
     document.getElementById("screen-on").style.display = "block";
     document.getElementById("white-bar").style.display = "block";
     document.getElementById("clock").style.display = "block";
+    document.getElementById("date").style.display = "block"
 }
 
 
 function showPasscode() {
     document.getElementById("clock").style.display = "none";
+    document.getElementById("date").style.display = "none"
     document.getElementById("password-screen").style.display = "block";
     document.getElementById("passcode-number-1").style.display = "block";
     document.getElementById("passcode-number-2").style.display = "block";
@@ -99,3 +124,14 @@ function logIn() {
     document.getElementById("passcode-number-0").style.display = "none";
     document.getElementById("miniClock").style.display = "block"
 }
+
+
+
+// const date = new Date();
+// 	var options = {
+//   weekday: 'long',
+//   month: 'long',
+//   day: 'numeric'
+// };
+
+// 	console.log(date.toLocaleDateString("default", options));

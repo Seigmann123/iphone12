40adda11b7996dfc83657eae23899d365fe44c23
Sander Seiersnes <sander.seiersnes@gmail.com>
2021-04-07 14:21:33 +0200

    njkn

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,3 @@
-
 function activateScreen() {
     var o = document.getElementById("scree");
     if (o === document.getElementById("screen")) {
@@ -113,8 +112,20 @@ function logIn() {
     document.getElementById("app4").style.display = "block";
     document.getElementById("white-bar").style.display = "none";
     document.getElementById("white-bar2").style.display = "none";
+    document.getElementsByClassName("calculator")[0].style.display = "none"
+    document.getElementsByClassName("input")[0].style.display = "none"
+    document.getElementsByClassName("pluss")[0].style.display = "none"
+    document.getElementsByClassName("minus")[0].style.display = "none"
+    document.getElementsByClassName("multiply")[0].style.display = "none"
+    document.getElementsByClassName("dot")[0].style.display = "none"
+    document.getElementById("null").style.display = "none"
+    document.getElementById("plussminus").style.display = "none"
+    document.getElementById("prosent").style.display = "none"
+    document.getElementsByClassName("operators")[0].style.display = "none"
+    document.getElementsByClassName("numbers")[0].style.display = "none"
+    document.getElementsByClassName("equal")[0].style.display = "none"
+    document.getElementById("clear").style.display = "none"
 
-    
 }
 
 
@@ -122,138 +133,138 @@ function logIn() {
 function calculator() {
 
 
-document.getElementById("app1").style.display = "none";
-document.getElementById("app2").style.display = "none";
-document.getElementById("app3").style.display = "none";
-document.getElementById("app4").style.display = "none";
-document.getElementById("login").style.display = "none";
-document.getElementById("white-bar2").style.display = "block";
+    document.getElementById("app1").style.display = "none";
+    document.getElementById("app2").style.display = "none";
+    document.getElementById("app3").style.display = "none";
+    document.getElementById("app4").style.display = "none";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("white-bar2").style.display = "block";
 
-document.getElementsByClassName("calculator")[0].style.display = "block"
-document.getElementsByClassName("input").style.display = "block"
-document.getElementsByClassName("pluss").style.display = "block"
-document.getElementsByClassName("minus").style.display = "block"
-document.getElementsByClassName("multiply").style.display = "block"
-document.getElementsByClassName("dot").style.display = "block"
-document.getElementById("null").style.display = "block"
-document.getElementById("plussminus").style.display = "block"
-document.getElementById("prosent").style.display = "block"
-document.getElementsByClassName("operators").style.display = "block"
-document.getElementsByClassName("numbers").style.display = "block"
-document.getElementsByClassName("equal").style.display = "block"
-document.getElementById("clear").style.display = "block"
+    document.getElementsByClassName("calculator")[0].style.display = "block"
+    document.getElementsByClassName("input")[0].style.display = "block"
+    document.getElementsByClassName("pluss")[0].style.display = "block"
+    document.getElementsByClassName("minus")[0].style.display = "block"
+    document.getElementsByClassName("multiply")[0].style.display = "block"
+    document.getElementsByClassName("dot")[0].style.display = "block"
+    document.getElementById("null").style.display = "block"
+    document.getElementById("plussminus").style.display = "block"
+    document.getElementById("prosent").style.display = "block"
+    document.getElementsByClassName("operators")[0].style.display = "block"
+    document.getElementsByClassName("numbers")[0].style.display = "block"
+    document.getElementsByClassName("equal")[0].style.display = "block"
+    document.getElementById("clear").style.display = "block"
 
 
 
 
     var input = document.getElementById("input"),
-    number = document.querySelectorAll(".numbers div"),
-    operator = document.querySelectorAll(".operators div"),
-    result = document.getElementById("result"),
-    clear = document.getElementById("clear"),
-    resultDisplayed = false;
+        number = document.querySelectorAll(".numbers div"),
+        operator = document.querySelectorAll(".operators div"),
+        result = document.getElementById("result"),
+        clear = document.getElementById("clear"),
+        resultDisplayed = false;
 
-for (var i = 0; i < number.length; i++) {
-    number[i].addEventListener("click", function (e) {
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
+    for (var i = 0; i < number.length; i++) {
+        number[i].addEventListener("click", function(e) {
+            var currentString = input.innerHTML;
+            var lastChar = currentString[currentString.length - 1];
 
-        if (resultDisplayed === false) {
-            input.innerHTML += e.target.innerHTML;
-        } else if (
-            (resultDisplayed === true && lastChar === "+") ||
-            lastChar === "-" ||
-            lastChar === "×" ||
-            lastChar === "÷"
-        ) {
-            resultDisplayed = false;
-            input.innerHTML += e.target.innerHTML;
-        } else {
-            resultDisplayed = false;
-            input.innerHTML = "";
-            input.innerHTML += e.target.innerHTML;
+            if (resultDisplayed === false) {
+                input.innerHTML += e.target.innerHTML;
+            } else if (
+                (resultDisplayed === true && lastChar === "+") ||
+                lastChar === "-" ||
+                lastChar === "×" ||
+                lastChar === "÷"
+            ) {
+                resultDisplayed = false;
+                input.innerHTML += e.target.innerHTML;
+            } else {
+                resultDisplayed = false;
+                input.innerHTML = "";
+                input.innerHTML += e.target.innerHTML;
+            }
+        });
+    }
+
+    for (var i = 0; i < operator.length; i++) {
+        operator[i].addEventListener("click", function(e) {
+            var currentString = input.innerHTML;
+            var lastChar = currentString[currentString.length - 1];
+
+            if (
+                lastChar === "+" ||
+                lastChar === "-" ||
+                lastChar === "×" ||
+                lastChar === "÷"
+            ) {
+                var newString =
+                    currentString.substring(0, currentString.length - 1) +
+                    e.target.innerHTML;
+                input.innerHTML = newString;
+            } else if (currentString.length == 0) {
+                console.log("enter a number first");
+            } else {
+                input.innerHTML += e.target.innerHTML;
+            }
+        });
+    }
+
+    result.addEventListener("click", function() {
+        var inputString = input.innerHTML;
+
+        var numbers = inputString.split(/\+|\-|\×|\÷/g);
+
+
+        var operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+        console.log(inputString);
+        console.log(operators);
+        console.log(numbers);
+        console.log("----------------------------");
+
+
+
+        var divide = operators.indexOf("÷");
+        while (divide != -1) {
+            numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+            operators.splice(divide, 1);
+            divide = operators.indexOf("÷");
         }
-    });
-}
 
-for (var i = 0; i < operator.length; i++) {
-    operator[i].addEventListener("click", function (e) {
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
-
-        if (
-            lastChar === "+" ||
-            lastChar === "-" ||
-            lastChar === "×" ||
-            lastChar === "÷"
-        ) {
-            var newString =
-                currentString.substring(0, currentString.length - 1) +
-                e.target.innerHTML;
-            input.innerHTML = newString;
-        } else if (currentString.length == 0) {
-            console.log("enter a number first");
-        } else {
-            input.innerHTML += e.target.innerHTML;
+        var multiply = operators.indexOf("×");
+        while (multiply != -1) {
+            numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+            operators.splice(multiply, 1);
+            multiply = operators.indexOf("×");
         }
+
+        var subtract = operators.indexOf("-");
+        while (subtract != -1) {
+            numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+            operators.splice(subtract, 1);
+            subtract = operators.indexOf("-");
+        }
+
+        var add = operators.indexOf("+");
+        while (add != -1) {
+
+            numbers.splice(
+                add,
+                2,
+                parseFloat(numbers[add]) + parseFloat(numbers[add + 1])
+            );
+            operators.splice(add, 1);
+            add = operators.indexOf("+");
+        }
+
+        input.innerHTML = numbers[0];
+
+        resultDisplayed = true;
     });
-}
-
-result.addEventListener("click", function () {
-    var inputString = input.innerHTML;
-
-    var numbers = inputString.split(/\+|\-|\×|\÷/g);
 
 
-    var operators = inputString.replace(/[0-9]|\./g, "").split("");
-
-    console.log(inputString);
-    console.log(operators);
-    console.log(numbers);
-    console.log("----------------------------");
-
-
-
-    var divide = operators.indexOf("÷");
-    while (divide != -1) {
-        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-        operators.splice(divide, 1);
-        divide = operators.indexOf("÷");
-    }
-
-    var multiply = operators.indexOf("×");
-    while (multiply != -1) {
-        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-        operators.splice(multiply, 1);
-        multiply = operators.indexOf("×");
-    }
-
-    var subtract = operators.indexOf("-");
-    while (subtract != -1) {
-        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-        operators.splice(subtract, 1);
-        subtract = operators.indexOf("-");
-    }
-
-    var add = operators.indexOf("+");
-    while (add != -1) {
-
-        numbers.splice(
-            add,
-            2,
-            parseFloat(numbers[add]) + parseFloat(numbers[add + 1])
-        );
-        operators.splice(add, 1);
-        add = operators.indexOf("+");
-    }
-
-    input.innerHTML = numbers[0];
-
-    resultDisplayed = true;
-});
-
-
-clear.addEventListener("click", function () {
-    input.innerHTML = "";
-});
+    clear.addEventListener("click", function() {
+        input.innerHTML = "";
+    });
 }
